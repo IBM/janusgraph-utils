61eabd424f9b4cca1c3a3b8b032b630f0b565224
yhwang <yh.wang@ibm.com>
2017-04-26 16:06:05 -0700

    Use Bean object to map JSON
    Refactor the JSON parsing code to use jackson-databind and also
    support more feature when creating vertex and edge
    
    Signed-off-by: yhwang <yh.wang@ibm.com>

--- README.md
+++ README.md
@@ -25,3 +25,89 @@ gremlin> importer = new JanusgraphGSONSchema(graph)
 gremlin> importer.readFile('schema.json')
 ```
 
+#### schema GSON
+```
+{
+    "propertyKeys": [.....],
+    "vertexLabels": [.....],
+    "edgeLabels": [.....],
+    "vertexIndexes": [.....],
+    "edgeIndexes": [.....],
+    "vertexCentricIndexes": [.....]
+}
+```
+
+Each property contains an array of objects. See detailed information for each 
+kind of object below.
+
+#### schema for propertyKey
+
+The schema of the object inside the array of "propertyKeys":
+```
+{
+    "name": "<propertyName>",
+    "dataType": "<String | Long | Character | Boolean | Byte | Short | Integer | Long | Float | Geoshape | UUID | Date>",
+    "cardinality": "<SINGLE|LIST|SET>"
+}
+```
+
+#### schema for VertexLabel
+
+The schema of the object inside the array of "vertexLabels":
+```
+{
+    "name": "<vertex label>",
+    "partition": false|true,    //optional
+    "useStatic": false|true     //optional
+}
+```
+
+#### schema for EdgeLabel
+
+The schema of the object inside the array of "edgeLabels":
+```
+{
+    "name": "<edge label>",
+    "multiplicity": "<MULTI | SIMPLE | ONE2MANY | MANY2ONE | ONE2ONE>"
+    "signatures": [ "<property key name>" ]
+    "unidirected" : true|false    //default value is false, means directed
+}
+```
+
+#### schema for vertexIndexes
+
+The schema of the object inside of the array of "vertexIndexes":
+```
+{
+    "name": "<index name>",
+    "propertyKeys": ["<property key name>"],
+    "composite": true|false,
+    "unique": true|false,
+    "indexOnly": "<vertex label>"
+}
+```
+
+#### schema for edgeIndexes
+
+The schema of the object inside of the array of "edgeIndexes":
+```
+{
+    "name": "<index name>",
+    "propertyKeys": ["<property key name>"],
+    "composite": true|false,
+    "indexOnly": "<edge label>"
+}
+```
+
+#### schema for vertexCentricIndexes
+
+The schema of the object inside of the array of "vertexCentricIndexes":
+```
+{
+    "name": "<index name>",
+    "propertyKeys": ["<property key>"],
+    "edge": "<edge label>",
+    "direction": "BOTH|IN|OUT",
+    "order": "incr|decr"
+}
+```
--- src/JanusgraphGSONSchema.groovy
+++ src/JanusgraphGSONSchema.groovy
@@ -8,39 +8,279 @@ import org.apache.tinkerpop.shaded.jackson.databind.node.TextNode
 import org.janusgraph.core.Cardinality
 import org.janusgraph.core.EdgeLabel
 import org.janusgraph.core.JanusGraph
+import org.janusgraph.core.Multiplicity
 import org.janusgraph.core.PropertyKey
 import org.janusgraph.core.attribute.Geoshape
+import org.janusgraph.core.schema.EdgeLabelMaker
 import org.janusgraph.core.schema.JanusGraphManagement
 import org.janusgraph.core.schema.JanusGraphSchemaType
+import org.janusgraph.core.schema.VertexLabelMaker
 import org.janusgraph.core.schema.JanusGraphManagement.IndexBuilder
 
 /**
- * A utility class to read GraphSON schema document and write to JanusGraph
+ * Janusgraph datatype mapping table
  */
-class JanusgraphGSONSchema {
-    JanusGraph graph
-    File gsonFile
-    static HashMap<String, Class> sTypeMap = new HashMap()
-
+class TypeMap {
+    public static HashMap<String, Class> MAP = new HashMap<String, Class>()
     /**
      * Create the data type mapping table here
      * note: can't find the corresponding classes for:
      *       Decimal and Precision
      */
     static {
-        sTypeMap.put("String", String.class)
-        sTypeMap.put("Character", Character.class)
-        sTypeMap.put("Boolean", Boolean.class)
-        sTypeMap.put("Byte", Byte.class)
-        sTypeMap.put("Short", Short.class)
-        sTypeMap.put("Integer", Integer.class)
-        sTypeMap.put("Long", Long.class)
-        sTypeMap.put("Float", Float.class)
-        sTypeMap.put("Geoshape", Geoshape.class)
-        sTypeMap.put("UUID", UUID.class)
-        sTypeMap.put("Date", Date.class)
-
+        MAP.put("String", String.class)
+        MAP.put("Character", Character.class)
+        MAP.put("Boolean", Boolean.class)
+        MAP.put("Byte", Byte.class)
+        MAP.put("Short", Short.class)
+        MAP.put("Integer", Integer.class)
+        MAP.put("Long", Long.class)
+        MAP.put("Float", Float.class)
+        MAP.put("Geoshape", Geoshape.class)
+        MAP.put("UUID", UUID.class)
+        MAP.put("Date", Date.class)
     }
+}
+/**
+ * JavaBean for propertyKey
+ */
+class PropertyKeyBean {
+    String name = null
+    String dataType = null
+    String cardinality = null
+
+    /**
+     * Create the propertyKey
+     * @param mgmt
+     */
+    public void make(JanusGraphManagement mgmt) {
+        if (name == null) {
+            println "need \"name\" property to define a propertyKey"
+        } else if (mgmt.containsPropertyKey(name)) {
+            println "property: ${name} exists"
+        } else {
+            try {
+                mgmt.makePropertyKey(name)
+                    .dataType(TypeMap.MAP.get(dataType))
+                    .cardinality(Cardinality.valueOf(cardinality)).make()
+                println "propertyKey:${name} creation is done"
+            } catch (Exception e) {
+                println "can't create property:${name}, ${e.getMessage()}"
+            }
+        }
+    }
+}
+
+/**
+ * Javabean for vertexLabel
+ *
+ */
+class VertexLabelBean {
+    String name = null
+    boolean partition = false
+    boolean useStatic = false
+
+    /**
+     * Create the vertex
+     * @param mgmt
+     */
+    public void make(JanusGraphManagement mgmt) {
+        if (name == null) {
+            println "need \"name\" property to define a vertex"
+        } else if (mgmt.containsVertexLabel(name)) {
+            println "vertex: ${name} exists"
+        } else {
+            try {
+                VertexLabelMaker maker = mgmt.makeVertexLabel(name)
+                if (partition) maker.partition()
+                if (useStatic) maker.setStatic()
+                maker.make()
+                println "vertex:${name} creation is done"
+            } catch (Exception e) {
+                println "can't create vertex: ${name}, ${e.getMessage()}"
+            }
+        }
+    }
+}
+
+/**
+ * JavaBean for EdgeLabel
+ *
+ */
+class EdgeLabelBean {
+    String name = null
+    String multiplicity = "MULTI"
+    List<String> signatures
+    boolean unidirected = false
+
+    public void make(JanusGraphManagement mgmt) {
+        if (name == null) {
+            println "need \"name\" property to define a label"
+        } else if (mgmt.containsEdgeLabel(name)) {
+            println "edge: ${name} exists"
+        } else {
+            try {
+                EdgeLabelMaker maker = mgmt.makeEdgeLabel(name).multiplicity(Multiplicity.valueOf(multiplicity))
+                if (signatures && signatures.size()) {
+                    PropertyKey[] keys = new PropertyKey[signatures.size()]
+                    int counter = 0;
+                    for (key in signatures) {
+                        keys[counter++] = mgmt.getPropertyKey(key);
+                    }
+                    maker.signature(keys)
+                }
+                if (unidirected) {
+                    maker.unidirected()
+                }
+                maker.make()
+                println "edge: ${name} creation is done"
+            } catch (Exception e) {
+                println "cant't create edge: ${name}, ${e.getMessage()}"
+            }
+        }
+    }
+}
+
+/**
+ * JavaBean for Index, both vertex and edge index
+ *
+ */
+class IndexBean {
+    String name = null
+    List<String> propertyKeys
+    boolean composite = true
+    boolean unique = false
+    String indexOnly = null
+    String mixedIndex = null
+
+    /**
+     * Create vertex/edge index based on the {@code node}. If {@code isVertexIndex} equals true
+     * a vertex index is created. Otherwise, create edge index
+     *
+     * @param mgmt is used to call the {@code buildIndex()}
+     * @param isVertexIndex create a vertex index or edge index
+     */
+    public void make(JanusGraphManagement mgmt, boolean isVertexIndex) {
+        if (name == null) {
+            println "missing the 'name' property, not able to create an index"
+            return
+        }
+
+        if (mgmt.containsGraphIndex(name)) {
+            println "index: ${name} exists"
+            return
+        }
+
+        if (propertyKeys == null || propertyKeys.size() == 0) {
+            println "missing the 'propertyKeys property, not able to create an index"
+            return
+        }
+
+        IndexBuilder ib = mgmt.buildIndex(name, isVertexIndex ? Vertex.class : Edge.class)
+        for (property in propertyKeys) {
+            ib.addKey(mgmt.getPropertyKey(property))
+        }
+
+        if (isVertexIndex && unique) {
+            ib.unique()
+        }
+
+        //indexOnly
+        if (indexOnly != null) {
+            JanusGraphSchemaType key = null
+            if (isVertexIndex) {
+                key = mgmt.getVertexLabel(indexOnly)
+            } else {
+                key = mgmt.getEdgeLabel(indexOnly)
+            }
+
+            if (key == null) {
+                println "${indexOnly} doesn't exist, skip only property"
+            } else {
+                ib.indexOnly(key)
+            }
+        }
+
+        if (composite) {
+            ib.buildCompositeIndex()
+        }
+
+        if (mixedIndex != null) {
+            ib.buildMixedIndex(mixedIndex)
+        }
+
+        println "index: ${name} creation is done"
+    }
+}
+
+class VertexCentricIndexBean {
+    String name = null
+    String edge = null
+    List<String> propertyKeys
+    String order = "incr"
+    String direction = "BOTH"
+
+    /**
+     * Create vertex-centric index
+     *
+     * {
+     *     "name": "indexName",
+     *     "edge": "edgeLebel",
+     *     "propertyKeys": [ "propertyKey1", "propertyKey2" ],
+     *     "order": "incr|decr",
+     *     "direction": "BOTH|IN|OUT"
+     * }
+     */
+    public void make(JanusGraphManagement mgmt) {
+        if (name == null) {
+            println "missing 'name' property, not able to create a vertex-centric index"
+            return
+        }
+
+        if (edge == null) {
+            println "vertex-centric index needs 'edge' property to specify a edge label"
+            return
+        }
+
+        EdgeLabel elabel = mgmt.getEdgeLabel(edge)
+
+        if (elabel == null) {
+            println "edge: ${edge} doesn't exist"
+            return
+        }
+
+        if (mgmt.containsRelationIndex(elabel, name)) {
+            println "vertex-centric index: ${name} exists"
+            return
+        }
+
+        if (propertyKeys == null || propertyKeys.size() == 0) {
+            println "missing 'propertyKeys property, not able to create an index"
+            return
+        }
+
+        PropertyKey[] keys = new PropertyKey[propertyKeys.size()]
+        int counter = 0
+        for (property in propertyKeys) {
+            PropertyKey key = mgmt.getPropertyKey(property)
+            if (key == null) {
+                println "propertyKey:${property} doesn't exist, can't create ${name} vertex-centric index"
+                return
+            }
+            keys[counter++] = mgmt.getPropertyKey(property)
+        }
+
+        mgmt.buildEdgeIndex(elabel, name, Direction.valueOf(direction), Order.valueOf(order), keys)
+
+        println "vertex-centric index: ${name} creation is done"
+    }
+}
+
+/**
+ * A utility class to read GraphSON schema document and write to JanusGraph
+ */
+class JanusgraphGSONSchema {
+    JanusGraph graph
 
     /**
      * Constructor of JansugraphGSONSchema object with the {@code graph}
@@ -60,87 +300,88 @@ class JanusgraphGSONSchema {
      *        IBM Graph GraphSON schema format.
      */
     public void readFile(String gsonSchemaFile) {
-        gsonFile = new File(gsonSchemaFile)
+        File gsonFile = new File(gsonSchemaFile)
         ObjectNode root = null
 
         if (!gsonFile.exists()) {
             throw new Exception("file not found:" + gsonSchemaFile)
         }
 
+        ObjectMapper mapper = new ObjectMapper()
+
         try {
-            ObjectMapper mapper = new ObjectMapper()
             root = mapper.readTree(gsonFile)
         } catch (Exception e) {
-            throw new Exception("can't parse GSON file:" + e.getMessage())
+            print "parse GSON failed: ${e.getMessage()}"
+            return
         }
 
         JanusGraphManagement mgmt = graph.openManagement()
 
         if (root.has("propertyKeys")) {
             for (node in root.get("propertyKeys").asList()) {
-                String name = node.get("name").asText()
-                if (mgmt.containsPropertyKey(name)) {
-                    println "property: ${name} exists"
-                } else {
-                    try {
-                        mgmt.makePropertyKey(node.get("name").asText())
-                            .dataType(sTypeMap.get(node.get("dataType").asText()))
-                            .cardinality(Cardinality.valueOf(node.get("cardinality").asText())).make()
-                        println "propertyKey:${name} creation is done"
-                    } catch (Exception e) {
-                        println "can't create property:${name}, ${e.getMessage()}"
-                    }
+                //use JSON ==> POJO in jackson-databind
+                try {
+                    mapper.convertValue(node, PropertyKeyBean.class)
+                        .make(mgmt)
+                } catch (Exception e) {
+                    println "incorrect propertyKey format: ${e.getMessage()}"
                 }
             }
         }
 
         if (root.has("vertexLabels")) {
             for (vertex in root.get("vertexLabels").asList()) {
-                String name = vertex.get("name").asText()
-                if (mgmt.containsVertexLabel(name)) {
-                    println "vertex: ${name} exists"
-                } else {
-                    try {
-                        mgmt.makeVertexLabel(name).make()
-                        println "vertex:${name} creation is done"
-                    } catch (Exception e) {
-                        println "can't create vertex: ${name}, ${e.getMessage()}"
-                    }
+                try {
+                    mapper.convertValue(vertex, VertexLabelBean.class)
+                        .make(mgmt)
+                } catch (Exception e) {
+                    println "incorrect vertex label format: ${e.getMessage()}"
                 }
             }
         }
 
         if (root.has("edgeLabels")) {
             for (edge in root.get("edgeLabels").asList()) {
-                String name = edge.get("name").asText()
-                if (mgmt.containsEdgeLabel(name)) {
-                    println "edge: ${name} exists"
-                } else {
-                    try {
-                        mgmt.makeEdgeLabel(name).make()
-                        println "edge:${name} creation is done"
-                    } catch (Exception e) {
-                        println "cant't create edge: ${name}, ${e.getMessage()}"
-                    }
+                try {
+                    mapper.convertValue(edge, EdgeLabelBean.class)
+                        .make(mgmt)
+                } catch (Exception e) {
+                    e.printStackTrace()
                 }
             }
         }
 
         if (root.has("vertexIndexes")) {
             for (vindex in root.get("vertexIndexes").asList()) {
-                makeIndex(mgmt, vindex, true)
+                try {
+                    mapper.convertValue(vindex, IndexBean.class)
+                        .make(mgmt, true)
+                } catch (Exception e) {
+                    println "incorrect vetex index format ${e.getMessage()}"
+                }
             }
         }
 
         if (root.has("edgeIndexes")) {
             for (eindex in root.get("edgeIndexes").asList()) {
-                makeIndex(mgmt, eindex, false)
+                try {
+                    mapper.convertValue(eindex, IndexBean.class)
+                        .make(mgmt, false)
+                } catch (Exception e) {
+                    println "incorrect edge index format: ${e.getMessage()}"
+                }
             }
         }
 
         if (root.has("vertexCentricIndexes")) {
             for (ecindex in root.get("vertexCentricIndexes").asList()) {
-                makeVCIndex(mgmt, ecindex)
+                try {
+                    mapper.convertValue(ecindex, VertexCentricIndexBean.class)
+                        .make(mgmt)
+                } catch (Exception e) {
+                    println "incorrect vertex-centric index format: ${e.getMessage()}"
+                }
             }
         }
 
@@ -158,132 +399,5 @@ class JanusgraphGSONSchema {
         }
     }
 
-    /**
-     * Create vertex/edge index based on the {@code node}. If {@code isVertexIndex} equeals true
-     * a vertex index is created. Otherwise, create edge index
-     *
-     * @param mgmt is used to call the {@code buildIndex()}
-     * @param node contains "name", "propertyKeys", "unique", "composite" and "mixedIndex" properties
-     * @param isVertexIndex create a vertex index or edge index
-     */
-    void makeIndex(JanusGraphManagement mgmt, ObjectNode node, boolean isVertexIndex) {
-        if (!node.has("name")) {
-            println "missing the 'name' property, not able to create an index"
-            return
-        }
 
-        String name = node.get("name").asText()
-        if (mgmt.containsGraphIndex(name)) {
-            println "index: ${name} exists"
-            return
-        }
-
-        if (!node.has("propertyKeys")) {
-            println "missing the 'propertyKeys property, not able to create an index"
-            return
-        }
-
-        IndexBuilder ib = mgmt.buildIndex(node.get("name").asText(), isVertexIndex ? Vertex.class : Edge.class)
-        List<TextNode> properties = node.get("propertyKeys").asList()
-        for (property in properties) {
-            ib.addKey(mgmt.getPropertyKey(property.asText()))
-        }
-
-        if (node.has("unique") && node.get("unique").asBoolean()) {
-            ib.unique()
-        }
-
-        //indexOnly
-        if (node.has("only")) {
-            JanusGraphSchemaType key = null
-            String onlyName = node.get("only").asText()
-            if (isVertexIndex) {
-                key = mgmt.getVertexLabel(onlyName)
-            } else {
-                key = mgmt.getEdgeLabel(onlyName)
-            }
-
-            if (key == null) {
-                println "${onlyName} doesn't exist, skip only property"
-            } else {
-                ib.indexOnly(key)
-            }
-        }
-
-        if (node.has("composite") && node.get("composite").asBoolean()) {
-            ib.buildCompositeIndex()
-        }
-
-        if (node.has("mixedIndex")) {
-            ib.buildMixedIndex(node.get("mixedIndex").asText())
-        }
-
-        println "index: ${name} creation is done"
-    }
-
-    /**
-     * Create vertex-centric index
-     *
-     * {
-     *     "name": "indexName",
-     *     "edge": "edgeLebel",
-     *     "propertyKeys": [ "propertyKey1", "propertyKey2" ],
-     *     "order": "incr|decr",
-     *     "direction": "BOTH|IN|OUT"
-     * }
-     */
-    void makeVCIndex(JanusGraphManagement mgmt, ObjectNode node) {
-        if (!node.has("name")) {
-            println "missing 'name' property, not able to create an index"
-            return
-        }
-
-        if (!node.has("edge")) {
-            println "vertex-centric index needs 'edge' property to specify a edge label"
-            return
-        }
-
-        String edgeName = node.get("edge").asText()
-        EdgeLabel elabel = mgmt.getEdgeLabel(edgeName)
-
-        if (elabel == null) {
-            println "edge: ${edgeName} doesn't exist"
-            return
-        }
-
-        String name = node.get("name").asText()
-        if (mgmt.containsRelationIndex(elabel, name)) {
-            println "index: ${name} exists"
-            return
-        }
-
-        if (!node.has("propertyKeys")) {
-            println "missing 'propertyKeys property, not able to create an index"
-            return
-        }
-
-        List<TextNode> properties = node.get("propertyKeys").asList()
-        if (properties == null || properties.size() == 0) {
-            println "index: ${name} needs 'propertyKeys' properties"
-            return
-        }
-
-        Direction dir = node.has("direction") ? Direction.valueOf(node.get("direction").asText()) : Direction.BOTH
-        Order order = node.has("order") ? Order.valueOf(node.get("order").asText()) : Order.incr
-
-        PropertyKey[] keys = new PropertyKey[properties.size()]
-        int counter = 0
-        for (property in properties) {
-            PropertyKey key = mgmt.getPropertyKey(property.asText())
-            if (key == null) {
-                println "propertyKey:${property.asText()} doesn't exist, can't create ${name} vertex-centric index"
-                return
-            }
-            keys[counter++] = mgmt.getPropertyKey(property.asText())
-        }
-
-        mgmt.buildEdgeIndex(elabel, name, dir, order, keys)
-
-        println "vertex-centric index: ${name} creation is done"
-    }
 }
